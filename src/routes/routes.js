1574eb1dac971cbf1b56ab489868e1cac69ee2b7
marcio reis <reismarcio143@gmail.com>
2026-09-14 14:58:56 -0300

    desenvolvimento das rotas

---
+++ src/routes/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const livrosController = require('./livrosController');
+const resenhasController = require('./resenhasController');
+const validarResenha = require('./validarResenha');
+
+// Rotas de livros
+router.get('/livros/buscar', livrosController.buscar);
+router.post('/livros', livrosController.salvar);
+router.get('/livros/:livroId/resenhas', resenhasController.listarPorLivro);
+
+// Rotas de resenhas
+router.post('/resenhas', validarResenha, resenhasController.criar);
+router.put('/resenhas/:id', validarResenha, resenhasController.atualizar);
+router.delete('/resenhas/:id', resenhasController.deletar);
+
+// Rotas de usuários
+router.get('/usuarios/:usuarioId/resenhas', resenhasController.listarPorUsuario);
+
+module.exports = router;
